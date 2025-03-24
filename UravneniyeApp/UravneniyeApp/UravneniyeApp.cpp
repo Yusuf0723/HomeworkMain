7dcf56fdafbde083494f5ea4611798a1aaacdf5d
USER <aliyevyusuf54@gmail>
2025-03-24 15:08:03 +0400

    Beru

---
+++ UravneniyeApp/UravneniyeApp/UravneniyeApp.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+void beru() {
+    int num, b;
+    cout <<"Enter num:";
+    cin >> num;
+    if (num > 0) {
+        for (int i = 1; i <= 10; i++)
+        {
+            b = i * num;
+            cout << num << "*" << i << "=" << b <<endl;
+        }
+    }
+    else {
+        cout << "Error" << endl;
+    }
+}
+int main()
+{
+    beru();
+}
+
+
