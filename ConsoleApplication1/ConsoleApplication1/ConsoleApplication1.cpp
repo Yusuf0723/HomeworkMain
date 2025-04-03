5c7ff0098b8dc4f5afbb2998d67c71da7ba82ad6
USER <aliyevyusuf54@gmail>
2025-04-03 19:02:55 +0400

    Hw1.1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << "input:";
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		for (int p = 0; p < i; p++)
+		{
+			cout << "*";
+		}
+		cout << "*" << endl;
+	}
+	for (int j = num; j > 0; j--)
+	{
+		for (int f = 0; f < num; f++)
+		{
+			cout << " ";
+		}
+		cout << "*"<<endl;
+	}
+}
